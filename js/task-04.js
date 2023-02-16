f3a6c34e81268ebc7f0969219cc1eab6c4d0c404
Aleksandr Slyvka <aleksandrslivka@gmail.com>
2023-02-16 23:31:52 +0100

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,11 +1,27 @@
+// const downBtn = document.querySelector("[data-action= decrement]");
+// const upBtn = document.querySelector("[data-action= increment]");
+// const valueEl = document.querySelector("#value");
+
+// downBtn.addEventListener("click", () => {
+//   valueEl.textContent = +valueEl.textContent - 1;
+// });
+
+// upBtn.addEventListener("click", () => {
+//   valueEl.textContent = +valueEl.textContent + 1;
+// });
+
 const downBtn = document.querySelector("[data-action= decrement]");
 const upBtn = document.querySelector("[data-action= increment]");
 const valueEl = document.querySelector("#value");
 
+let valueCounter = 0;
+
 downBtn.addEventListener("click", () => {
-  valueEl.textContent = +valueEl.textContent - 1;
+  valueCounter = valueCounter - 1;
+  valueEl.textContent = valueCounter;
 });
 
 upBtn.addEventListener("click", () => {
-  valueEl.textContent = +valueEl.textContent + 1;
+  valueCounter = valueCounter + 1;
+  valueEl.textContent = valueCounter;
 });
